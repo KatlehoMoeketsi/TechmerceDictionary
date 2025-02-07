c61263b892acc5b0eb06a85e534548e5a676dc3a
KatlehoMoeketsi <kaydawg718@gmail.com>
2025-02-07 13:37:22 +0200

    Added part of speech to the definition

--- main.py
+++ main.py
@@ -28,19 +28,26 @@ def get_definition():
 
         if isinstance(data, list):
             meaning = data[0]['meanings'][0]['definitions'][0]['definition']
-            # print(meaning)
-            result_label.config(text=f"Definition: {meaning}")
+            part_of_speech = data[0]['meanings'][0]['partOfSpeech']
+            print(data[0]['meanings'][0]['partOfSpeech'])
+            result_label.config(text=f"({part_of_speech}) Definition: {meaning}")
         else:
             result_label.config(text="Word not found")
 
     except requests.exceptions.RequestException as e:
         messagebox.showerror("Error", f"Failed to fetch definition: {e}")
 
-
+#Center the window
+def center_window(window):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - window.winfo_reqwidth())//2
+    y = (screen_height - window.winfo_reqheight())//2
+    window.geometry(f"500x250+{x}+{y}")
 
 root = tk.Tk()
 root.title('TechmerceDictionary')
-
+center_window(root)
 
 #UI elements
 tk.Label(root, text="Enter a Word:"). pack(pady=5)
